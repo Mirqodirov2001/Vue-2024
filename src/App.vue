ae5478afd617e52577b5b0e1dfeec2bfcaf00db1
mirqodirov2001 <mirqodirov678@gmail.com>
2024-01-15 23:44:48 +0500

    save

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Cards from "./components/Cards.vue"
+</script>
+
+<template>
+  <div class="container">
+    <Cards/>
+  </div>
+</template>
+
+<style scoped>
+.card{
+  
+}
+</style>
